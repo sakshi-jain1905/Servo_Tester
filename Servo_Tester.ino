787ceadc8f7ef691599210cbc7d81f13f0c42363
Home PC <sakshijain19051999>
2018-01-14 18:24:49 +0530

    first commit

---
+++ Servo_Tester.ino
@@ -0,0 +1,22 @@
+
+
+#include <Servo.h>
+
+Servo myservo; 
+
+int potpin = 0;
+int val;    
+
+void setup() {
+  myservo.attach(9); 
+   Serial.begin(9600); 
+}
+
+void loop() {
+ myservo.attach(9);
+  val = analogRead(potpin);            
+  val = map(val, 0, 1023, 0, 180);     
+  myservo.write(val);                  
+  delay(15);        
+                   
+  }
